39580530187972d4fab414242c5013cadc422b98
paintingofblue <90877067+paintingofblue@users.noreply.github.com>
2022-08-02 18:58:00 +1200

    added discord to telegram

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import os
+import requests
+import urllib.parse
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN=os.getenv('TOKEN')
+token = os.getenv('token')
+telegram_token = os.getenv('telegram_token')
+chatid = os.getenv('chatid')
 bot = commands.Bot(command_prefix="!")
 
 @bot.event
-async def on_message():
-    print("")
+async def on_message(message):
+    global msg
+    global msg1
+    global msg2
+    msg1 =  urllib.parse.quote(f"{message.author.name}#"f"{message.author.discriminator} - " + f"{message.content}")
+    try:
+        if message.reference.cached_message != "None":
+            msg2 = urllib.parse.quote(f"Replying to: {message.reference.cached_message.content}")
+            requests.post(f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={chatid}&text={msg2}\n{msg1}")
+    except:
+        requests.post(f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={chatid}&text={msg1}")
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name}#{bot.user.discriminator} has connected to Discord!')
 
-
-bot.run(TOKEN)
+bot.run(token)
