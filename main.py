3ebeb3dec7e1f7eb772c51935af98d6997b83676
micgar19 <micgar19@kingshigh.school.nz>
2022-08-03 10:38:57 +1200

    added bridge between both

--- main.py
+++ main.py
@@ -1,30 +1,69 @@
 import os
 import requests
 import urllib.parse
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
+from discord_webhook import DiscordWebhook
 
 load_dotenv()
 token = os.getenv('token')
 telegram_token = os.getenv('telegram_token')
 chatid = os.getenv('chatid')
+webhookurl = os.getenv('webhook')
 bot = commands.Bot(command_prefix="!")
+previous = ''
+
+@tasks.loop(seconds=0.25)
+async def telegram_check():
+    global result
+    global previous
+    global r
+    r = requests.post(
+        f"https://api.telegram.org/bot{telegram_token}/getUpdates?offset=-1")
+    try:
+        result = r.json()['result'][0]['message']['text']
+        if result != previous:
+            webhook = DiscordWebhook(
+                url=webhookurl,
+                content=r.json()['result'][0]['message']['from']['username'] +
+                " - " + result)
+            webhook.execute()
+        elif result == previous:
+            pass
+        previous = result
+    except:
+        pass
+
 
 @bot.event
 async def on_message(message):
-    global msg
-    global msg1
-    global msg2
-    msg1 =  urllib.parse.quote(f"{message.author.name}#"f"{message.author.discriminator} - " + f"{message.content}")
-    try:
-        if message.reference.cached_message != "None":
-            msg2 = urllib.parse.quote(f"Replying to: {message.reference.cached_message.content}")
-            requests.post(f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={chatid}&text={msg2}\n{msg1}")
-    except:
-        requests.post(f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={chatid}&text={msg1}")
+    if message.author.bot == False:
+        global msg
+        global msg1
+        global msg2
+        msg1 = urllib.parse.quote(f"{message.author.name}#"
+                                  f"{message.author.discriminator} - " +
+                                  f"{message.content}")
+        try:
+            if message.reference.cached_message != "None":
+                msg2 = urllib.parse.quote(
+                    f"Replying to: {message.reference.cached_message.content}")
+                requests.post(
+                    f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={chatid}&text={msg2}\n{msg1}"
+                )
+        except:
+            requests.post(
+                f"https://api.telegram.org/bot{telegram_token}/sendMessage?chat_id={chatid}&text={msg1}"
+            )
+        else:
+            pass
+
 
 @bot.event
 async def on_ready():
-    print(f'{bot.user.name}#{bot.user.discriminator} has connected to Discord!')
+    print(
+        f'{bot.user.name}#{bot.user.discriminator} has connected to Discord!')
+    telegram_check.start()
+
 
 bot.run(token)
